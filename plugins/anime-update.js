25d996241df99ac84c32ede2abec0f47d8806cf2
Al-User12 <alfikrikm2@gmail.com>
2024-09-19 20:03:26 +0700

    Add anime update handler to fetch and display latest anime episodes

---
+++ plugins/anime-update.js
@@ -0,0 +1,40 @@
+let handler = async (m, { conn }) => {
+
+ function parseAnimeUpdates(html) {
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        const animeUpdates = [];
+      
+        const articles = document.querySelectorAll('article.animeseries');
+        articles.forEach((element) => {
+          const title = element.querySelector('h3.title span')?.textContent || '';
+          const link = element.querySelector('a')?.getAttribute('href') || '';
+          const episode = element.querySelector('span.types.episodes')?.textContent || '';
+          const imageUrl = element.querySelector('img')?.getAttribute('src') || '';
+      
+          animeUpdates.push({ title, link, episode, imageUrl });
+        });
+      
+        return animeUpdates;
+        }
+
+ async function fetchAnimeUpdates() {
+        const response = await fetch('https://nontonanimeid.autos/');
+        const body = await response.text();
+      
+        return parseAnimeUpdates(body);
+    }
+
+    const animeUpdates = await fetchAnimeUpdates();
+    const animeUpdate = animeUpdates[0];
+    const { title, link, episode, imageUrl } = animeUpdate;
+    const caption = `*${title}*\nEpisode: ${episode}\n\n${link}`;
+    conn.sendFile(groupId, imageUrl, 'anime.jpeg', caption, m);
+}
+
+handler.help = ['anime-today']
+handler.tags = ['anime']
+
+handler.command = /^anime-update$/i
+
+export default handler

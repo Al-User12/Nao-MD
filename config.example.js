9a6fee8865ebefa865bb4df42ca7e88321e0b70c
Al Fikri Kholil Misbah <44604935+Al-User12@users.noreply.github.com>
2025-04-28 17:32:33 +0700

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -30,34 +30,33 @@ const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 
 /*============= MAIN INFO =============*/
-global.pairing = '6285864034767'
-global.owner = [['6285174269046', 'ShirokamiRyzen', true]]
+global.owner = [['6282245744337', 'Archilez', true]]
 global.mods = []
 global.prems = []
-global.nomorbot = '6285864034767'
-global.nomorown = '6285174269046'
+global.nomorbot = '6281772317732'
+global.nomorown = '6285179714799'
 
 /*============= WATERMARK =============*/
 global.readMore = readMore
-global.author = 'Shirokami Ryzen'
-global.namebot = 'Nao-MD ESM'
-global.wm = '© Nao-MD By Shirokami Ryzen'
+global.author = 'Archilez'
+global.namebot = 'Archilez AI'
+global.wm = '© Archilez Tech'
 global.watermark = wm
 global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `T I M E : ${wktuwib}`
 global.stickpack = `Sticker Dibuat dengan ${namebot}\ngithub.com/ShirokamiRyzen\n\nNao-MD ESM\n+${nomorbot}`
-global.stickauth = `© Nao-MD By Shirokami Ryzen`
+global.stickauth = `© Archilez Tech`
 global.week = `${week} ${date}`
 global.wibb = `${wktuwib}`
 
 //*============= SOSMED =============*/
-global.sig = 'https://www.instagram.com/fatih_frdaus'
-global.sgh = 'https://github.com/ShirokamiRyzen'
-global.sgc = 'https://whatsapp.com/channel/0029Vb2qO9Q5vKAGd2t7ap1F'
-global.sgw = 'https://ryzendesu.vip'
+global.sig = 'https://www.instagram.com/yaelahfik'
+global.sgh = 'https://github.com/Al-User12'
+global.sgc = 'Coming Soon'
+global.sgw = 'https://al-user12.github.io'
 global.sdc = '-'
-global.sfb = 'https://www.facebook.com/Nao.Tomori.UwU'
-global.snh = 'https://www.instagram.com/fatih_frdaus'
+global.sfb = 'https://www.facebook.com/itsme.fikri'
+global.snh = 'https://www.instagram.com/yaelahfik'
 
 /*============= DONASI =============*/
 global.qris = 'https://api.ryzendesu.vip/images/qris.png'

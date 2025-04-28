0c8e454f164f859d87f6477f648e994332716041
Al Fikri Kholil Misbah <44604935+Al-User12@users.noreply.github.com>
2025-04-28 17:37:26 +0700

    Update owner-tagall.js

--- plugins/owner-tagall.js
+++ plugins/owner-tagall.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
 handler.help = ['o-tagall']
 handler.tags = ['owner']
 handler.command = ['o-tagall']
-handler.owner = false
+handler.owner = true
 handler.group = true
 handler.register = true
 export default handler
